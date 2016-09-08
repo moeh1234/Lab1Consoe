c3f7f0991821fd1ce09ea9eaffa6e5178ebfdb08
moeh1234 <moeh1234@gmail.com>
2016-09-08 13:31:02 -0400

    commit please

--- Lab1Consoe/Program.cs
+++ Lab1Consoe/Program.cs
@@ -10,6 +10,28 @@ namespace Lab1Consoe
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Welcome to Grand Circus' Room Detail Generator!\n");
+            string cont;
+            do
+            {
+                Console.Write("Enter Length: ");
+                int m = int.Parse(Console.ReadLine());
+                Console.Write("Enter Width: ");
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine("Area: " + m * n);
+                Console.WriteLine("Perimeter: " + (2 * m + 2 * n));
+                Console.WriteLine("Continue? (y/n)");
+                cont = Console.ReadLine();
+                Console.WriteLine();
+
+            } while (cont == "y");
+            Console.WriteLine("Goodye!");
+            
+
         }
     }
+
+
+
 }
